9c0745582df72390f93a56694e9dfec6f85bc2ba
trosman <tobias.rosman@gmail.com>
2022-02-14 13:33:14 +0100

    Show frontImageSmall

--- general.js
+++ general.js
@@ -175,12 +175,12 @@ function loadSoldByOthers(userID) {
                 var soldPrice = doc.data().soldPrice;
                 var images = doc.data().images;
                 var imageUrl = images.frontImage;
+                if (images.frontImageSmall){
+                    imageUrl = images.frontImageSmall;
+                }
 
                 // Add card to list if seller is other than myself
-                if (sellerId != userID && soldPrice >= 200) {
-                    if ("productImage" in images) {
-                        imageUrl = images.productImage;
-                    }
+                if (sellerId != userID && soldPrice >= 200) {                    
                     var soldByOthersItemCardHTML = `<div class="div-block-14"><div class="ratio-box _16-9"><div class="conten-block with-image"><div class="img-container" style="background-image: url('${imageUrl}');"></div></div></div><div class="text-block-14">${soldPrice} kr</div><div class='text-block-34'>${brand}</div></div>`;
                     itemListSoldByOthers.innerHTML += soldByOthersItemCardHTML;
                 }
