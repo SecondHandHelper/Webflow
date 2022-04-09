6ffae0fdf29ecba6cd49a8961b56f906a8609642
trosman <tobias.rosman@gmail.com>
2022-04-09 23:13:34 +0200

    triggerD

---
+++ infoRequestsFunctions.js
@@ -0,0 +1,118 @@
+async function openMeasurementsToast(itemId, description) {
+    measurementDescriptionText.innerHTML = description;
+    measurementsSubmitButton.addEventListener('click', async function () {
+        const input = measurementsIput.value;
+        if (input.length > 0) {
+            await db.collection('items').doc(itemId).update({
+                measurements: input,
+                "infoRequests.measurements.status": "Resolved"
+            });
+        }
+        triggerMeasurementsToastClose.click();
+        setTimeout(function () { location.reload(); }, 400);
+    });
+    triggerMeasurementsToastOpen.click();
+}
+
+async function openNewPriceToast(itemId, minPrice, maxPrice, text) {
+    newPriceText.innerHTML = text;
+    newPrice.innerHTML = `${minPrice}-${maxPrice} kr`;
+    acceptNewPriceButton.addEventListener('click', async function () {
+        await db.collection('items').doc(itemId).update({
+            "infoRequests.price.status": "Resolved",
+            "infoRequests.price.response": "Accepted",
+            "maxPriceEstimate": maxPrice,
+            "minPriceEstimate": minPrice
+        }).then(function () {
+            triggerNewPriceToastClose.click();
+            setTimeout(function () { location.reload(); }, 400);
+        });
+    });
+    denyNewPriceButton.addEventListener('click', async function () {
+        await db.collection('items').doc(itemId).update({
+            "infoRequests.price.status": "Resolved",
+            "infoRequests.price.response": "Denied"
+        }).then(function () {
+            triggerNewPriceToastClose.click();
+            setTimeout(function () { location.reload(); }, 400);
+        });
+    });
+    triggerNewPriceToastOpen.click();
+}
+
+function loadInfoRequests(userId) {
+    infoRequestsList.innerHTML = "";
+    db.collection("items").where("user", "==", userId).get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            var itemId = doc.id;
+            var infoRequests = doc.data().infoRequests;
+            var images = doc.data().images;
+            var status = doc.data().status;
+            var brand = doc.data().brand;
+            var minPriceEstimate = doc.data().minPriceEstimate;
+            var maxPriceEstimate = doc.data().maxPriceEstimate;
+            var acceptPrice = doc.data().acceptPrice;
+            var archived = doc.data().archived;
+            var frontImageUrl = images.frontImage;
+            if (images.frontImageSmall) {
+                frontImageUrl = images.frontImageSmall;
+            }
+            if (archived == undefined && status !== "Unsold" && status !== "Sold" && infoRequests) {
+                displayRequests();
+            }
+
+            function displayRequests() {
+                for (const req in infoRequests) {
+                    if (infoRequests[req]?.status === "Active") {
+                        let title = "";
+                        let subText = "";
+                        let href = "";
+                        let buttonText = `Komplettera`;
+                        let buttonClass = "completerequestbutton";
+                        let buttonTextClass = "text-block-69";
+                        let description = infoRequests[req].description;
+                        if (req === "price") {
+                            title = "Nytt pris";
+                            buttonClass = "acceptnewpricebutton";
+                            buttonTextClass = "text-block-69-copy-copy";
+                            buttonText = "Se pris";
+                            subText = "Accepterar du den nya prissättningen?";
+                            let max = infoRequests[req].maxPrice;
+                            let min = infoRequests[req].minPrice;
+                            let text = `Baserat på efterfrågan tror vi att priset för ditt ${brand}-plagg behöver gå under ditt lägsta accepterade pris för att öka chanserna att få det sålt.`;
+                            href = `javascript:openNewPriceToast('${itemId}', ${min}, ${max}, '${text}');`;
+                        }
+                        if (req === "measurements") {
+                            title = "Mått";
+                            subText = "Vi behöver mått för detta plagg";
+                            buttonText = "Ange mått";
+                            href = `javascript:openMeasurementsToast('${itemId}', '${description}');`;
+                        }
+                        if (req === "images") {
+                            title = "Bilder";
+                            subText = "Bilderna behöver kompletteras";
+                            buttonText = "Ändra bilder";
+                            href = window.location.origin + `/edit-item?id=${itemId}`;
+                        }
+                        let card = `<div class="div-block-126">
+                                                <div class="ratio-box _16-9">
+                                                    <div class="content-block with-image">
+                                                        <div class="img-container" style="background-image: url('${frontImageUrl}');"></div>
+                                                    </div>
+                                                </div>
+                                                <div class="text-block-73">${title}</div>
+                                                <div class="text-block-72">${subText}</div>
+                                                <a href="${href}" class="link-block-23 w-inline-block">
+                                                    <div class="${buttonClass}">
+                                                        <div class="${buttonTextClass}">${buttonText}</div>
+                                                    </div>
+                                                </a>
+                                            </div>`;
+                        infoRequestsList.innerHTML += card;
+                        infoRequestsDiv.style.display = "block";
+                    }
+                }
+            }
+        });
+    });
+}
