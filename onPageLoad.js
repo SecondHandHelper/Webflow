16f838c89c7704ced82317c5472d5789f319983b
trosman <trosman@users.noreply.github.com>
2022-05-18 14:52:00 +0200

    Update onPageLoad.js

--- onPageLoad.js
+++ onPageLoad.js
@@ -7,34 +7,3 @@ var domain = window.location.origin;
 if (!isMobile && !(path == "/home-desktop" || path == "/privacy-policy" || path == "/end-user-agreement" || path == "/private" || path == "/user-management") && !domain.includes("shh-test")) {
   window.location.replace('./home-desktop');
 }
-
-// GET USER
-var user;
-var authUser;
-firebase.auth().onAuthStateChanged((result) => {
-  if (result) {
-    authUser = result;
-
-    // Get and set currentUser
-    db.collection("users").doc(authUser.uid).get().then((doc) => {
-      if (doc.exists) {
-        user = doc.data();
-        console.log("user:", user);
-        fbq('init', '681886049842735', { 'external_id': authUser.uid }); fbq('track', 'PageView');
-        tryAttribution();
-        analytics.identify(`${authUser.uid}`, {
-          logged_in: true
-        });
-      }
-
-      if (typeof window.main === "function") {
-        main();
-      }
-    }).catch((error) => { console.log("Error getting document:", error); });
-  } else {
-    analytics.identify({
-      logged_in: false
-    });
-    console.log('No user');
-  }
-});
