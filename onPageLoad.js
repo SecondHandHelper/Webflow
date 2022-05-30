26df2b0c8c58e0199c100b60fddea7277ea743b7
trosman <tobias.rosman@gmail.com>
2022-05-30 14:25:01 +0200

    Remove redirect to home-desktop

--- onPageLoad.js
+++
@@ -1,9 +0,0 @@
-// REDIRECTS
-var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-var path = window.location.pathname;
-var domain = window.location.origin;
-
-// Redirect to Home Desktop if on desktop
-if (!isMobile && !(path == "/home-desktop" || path == "/privacy-policy" || path == "/end-user-agreement" || path == "/private" || path == "/user-management") && !domain.includes("shh-test")) {
-  window.location.replace('./home-desktop');
-}
