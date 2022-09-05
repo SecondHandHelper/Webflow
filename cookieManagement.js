f4ca474b031653564ad9d6a0c2f195556304da1c
trosman <tobias.rosman@gmail.com>
2022-09-05 09:02:14 +0200

    Fix encode / decode for cookie values

--- cookieManagement.js
+++ cookieManagement.js
@@ -2,6 +2,7 @@ function setCookie(cname, cvalue, exdays) {
     const d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
     let expires = "expires=" + d.toUTCString();
+    cvalue = encodeURIComponent(cvalue);
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
     console.log("Cookie set: ", cname + "=" + cvalue);
 }
@@ -15,7 +16,8 @@ function getCookie(cname) {
             c = c.substring(1);
         }
         if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
+            c = c.substring(name.length, c.length);
+            return decodeURIComponent(c);
         }
     }
     return "";
