77d19eb659492d32196652ea42ed3c78d2ffaa62
trosman <tobias.rosman@gmail.com>
2022-09-12 10:57:07 +0200

    Replace '+' with ' ' in cookie values

--- cookieManagement.js
+++ cookieManagement.js
@@ -2,6 +2,7 @@ function setCookie(cname, cvalue, exdays) {
     const d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
     let expires = "expires=" + d.toUTCString();
+    cvalue = cvalue.replaceAll('+', ' ');
     cvalue = encodeURIComponent(cvalue);
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
     console.log("Cookie set: ", cname + "=" + cvalue);
