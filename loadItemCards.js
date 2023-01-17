982805f01bcc16c4be3c6aea48d538d3b5d58551
trosman <tobias.rosman@gmail.com>
2023-01-17 15:52:01 +0100

    show full user address

--- loadItemCards.js
+++ loadItemCards.js
@@ -103,8 +103,10 @@ async function loadItemCards(items) {
 
           // Add a user action, such as 'show QR button' or 'bag received checkbox'
           if (shippingMethod === 'Service point') {
-            if (!bagReceived && (soldPlatform === 'Vestiaire Collective' || soldPlatform === 'Grailed')) {
-              userActionDiv = getBagReceivedCheckbox(itemId, soldDate);
+            if (soldPlatform === 'Vestiaire Collective' || soldPlatform === 'Grailed'){
+              if (!bagReceived) {
+                userActionDiv = getBagReceivedCheckbox(itemId, soldDate);
+              }
             }
             else if (postnordQrCode) {
               userActionDiv = getQrCodeButton(itemId);
