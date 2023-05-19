e544fad131187dff61cf418ecfa6636be9467bfb
Mikael Amborn <mikael.amborn@gmail.com>
2023-05-19 10:59:38 +0200

    Fix redirect after signin

--- signIn.js
+++ signIn.js
@@ -2,8 +2,11 @@ async function signedInNextStep() {
     // User is signed in
     // If itemCreatedFromAnotherItem in sessionStorage => Back to sell-item
     if (authUser) {
-      await updateFirestoreUserDocument(authUser.uid, email, phone); //Important that this happen first, since many other functions depend on an existing user document
+      const email = authUser.email || sessionStorage.getItem("email");
+      const phone = authUser.phoneNumber || sessionStorage.getItem("phoneNumber");
+      await updateFirestoreUserDocument(authUser.uid, email, phone); //Important that this happens first, since many other functions depend on an existing user document
     }
+    console.log({referrer: document.referrer});
     if (sessionStorage.getItem('itemToBeCreatedAfterSignIn') && document.referrer.includes('/sell-item')) {
         window.location.replace('./sell-item');
     } else {
