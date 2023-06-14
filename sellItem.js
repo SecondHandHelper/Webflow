7530d2c5426c9808970ca9e88de0d0bc336d91e1
Mikael Amborn <mikael.amborn@gmail.com>
2023-06-14 11:13:51 +0200

    Try without timeout

--- sellItem.js
+++ sellItem.js
@@ -417,9 +417,7 @@ async function initializeCategorySelect() {
   let openOptgroup = '';
   $('#itemCategory').select2({ selectionCssClass: 'form-field', placeholder: 'Kategori', data: itemCategories });
 
-  $('body').on('click', '.select2-container', () =>
-    setTimeout(() => $('.select2-search__field').focus(), 50)
-  );
+  $('body').on('click', '.select2-container', () => $('.select2-search__field').focus());
 
   $("body").on('click', '.select2-container--open .select2-results__group', function() {
     $(this).siblings().toggle();
