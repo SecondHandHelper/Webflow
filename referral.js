498126f03aed26cffabd25100954a02ea0435a9f
trosman <tobias.rosman@gmail.com>
2023-11-24 15:18:51 +0100

    referral changes

--- referral.js
+++ referral.js
@@ -6,24 +6,19 @@ async function main() {
   }
   document.getElementById('referralCode').innerText = user.current.referralData.referralCode;
   if (user.current?.referralData?.referredUsers?.length > 0) {
-      document.getElementById('topStats').classList.add("animatedBackground");
+      //TOP STATS
       document.getElementById('invitedFriends').innerText = user.current.referralData.referredUsers.length;
-      document.getElementById('topStatsDiv').style.display = 'flex';
       document.getElementById('referralDetails').style.display = 'block';
-      firebase.app().functions("europe-west1").httpsCallable('referralStats')().then(referralStatsResponse => {
-        const referralStats = referralStatsResponse.data;
-        document.getElementById('nextFreePill').style.display = referralStats.freeSells > referralStats.usedFreeSells ? 'block' : 'none';
-        document.getElementById('freeSells').innerText = referralStats.freeSells;
-        document.getElementById('usedFreeSells').innerText = referralStats.usedFreeSells;
-        document.getElementById('availableFreeSells').innerText = `/${referralStats.freeSells}`;
-      });
       const referredUserStatsResponse = await firebase.app().functions("europe-west1").httpsCallable('referredUserStats')();
       const referredUserStats = referredUserStatsResponse.data;
       document.getElementById('soldItems').innerText = `${referredUserStats.soldItems}`;
       // https://supermiljobloggen.se/nyheter/secondhand-200-ganger-mindre-klimatskadligt-an-nyproducerat/
       // 1 plagg orsakar 10 kg CO2 (2-17), och köpa begagnat orsakar 10/200 kg = 0.02kg så i genomsnitt 10kg sparat/plagg
       document.getElementById('savedCo2').innerText = (referredUserStats.soldItems * 10 / 1000).toLocaleString('sv');
-      
+      topStatsDiv.style.visibility = 'visible';
+      triggerShowReferralTopStats.click();
+
+      //INVITE STATS
       document.getElementById('invitesRegistered').innerText = referredUserStats.users?.length
       document.getElementById('invitesAddedItems').innerText = referredUserStats.users?.filter(usr => usr.status === 'activated' || usr.status === 'sold').length;
       document.getElementById('invitesSoldItems').innerText = referredUserStats.users?.filter(usr => usr.status === 'sold').length;
@@ -37,6 +32,13 @@ async function main() {
           newRow.childNodes[1].innerText = statusText(invitedFriend.status);
           invitedFriendStatusesDiv.appendChild(newRow);
       }
+      firebase.app().functions("europe-west1").httpsCallable('referralStats')().then(referralStatsResponse => {
+        const referralStats = referralStatsResponse.data;
+        document.getElementById('nextFreePill').style.display = referralStats.freeSells > referralStats.usedFreeSells ? 'block' : 'none';
+        document.getElementById('freeSells').innerText = referralStats.freeSells;
+        document.getElementById('usedFreeSells').innerText = referralStats.usedFreeSells;
+        document.getElementById('availableFreeSells').innerText = `/${referralStats.freeSells}`;
+      });
   }
 }
 
