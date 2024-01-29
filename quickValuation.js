4cb2ed6e1fc7f0c3a6a58993c9485e3f88e1fc63
Mikael Amborn <mikael.amborn@gmail.com>
2024-01-29 14:43:33 +0100

    Try and use input focus for category search as well

--- quickValuation.js
+++ quickValuation.js
@@ -1,5 +1,5 @@
 import {autocomplete, brands} from "./autocomplete-brands";
-import {fieldLabelToggle, initializeCategorySelect} from "./sellItemHelpers";
+import {fieldLabelToggle, focusAndOpenKeyboard, initializeCategorySelect} from "./sellItemHelpers";
 
 async function getValuation(itemBrand, itemCategory) {
   const brand = itemBrand.value ? itemBrand.value.trim() : "";
@@ -155,38 +155,6 @@ async function quickValuationMain() {
   };
   initializeCategorySelect('Skriv kategori här', () => { });
   itemBrand.addEventListener('input', fieldLabelToggle('itemBrandLabel'));
-  function focusAndOpenKeyboard(el, timeout) {
-    const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-    if (!isMobile) {
-      el.focus();
-      return;
-    }
-    if(!timeout) {
-      timeout = 100;
-    }
-    if(el) {
-      // Align temp input element approximately where the input element is
-      // so the cursor doesn't jump around
-      var __tempEl__ = document.createElement('input');
-      __tempEl__.style.position = 'absolute';
-      __tempEl__.style.top = (el.offsetTop + 7) + 'px';
-      __tempEl__.style.left = el.offsetLeft + 'px';
-      __tempEl__.style.height = 0;
-      __tempEl__.style.opacity = 0;
-      // Put this temp element as a child of the page <body> and focus on it
-      document.body.appendChild(__tempEl__);
-      __tempEl__.focus();
-
-      // The keyboard is open. Now do a delayed focus on the target element
-      setTimeout(function() {
-        el.focus();
-        el.click();
-        // Remove the temp element
-        document.body.removeChild(__tempEl__);
-      }, timeout);
-    }
-  }
-
 
   brandClearButton.addEventListener('click', () => {
     itemBrand.value = '';
--- sellItemHelpers.js
+++ sellItemHelpers.js
@@ -332,6 +332,7 @@ export function initializeCategorySelect(placeholderText = 'Kategori', onChangeC
     document.querySelector('html').style.overflow = 'fixed';
     const searchField = document.querySelector('.select2-search__field');
     searchField.placeholder = 'Sök... (t.ex. Klänning/Sneakers/Blus)';
+    focusAndOpenKeyboard(searchField, 100);
     $('.select2-search__field').on('input', (e) => {
       if (e.target.value.length > 0) {
         $('.select2-results__option[role=group]').each((idx, elm) => $(elm).addClass('expanded-group'));
@@ -367,6 +368,39 @@ export function initializeCategorySelect(placeholderText = 'Kategori', onChangeC
     }, 50);
   });
 }
+
+export function focusAndOpenKeyboard(el, timeout) {
+  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  if (!isMobile) {
+    el.focus();
+    return;
+  }
+  if(!timeout) {
+    timeout = 100;
+  }
+  if(el) {
+    // Align temp input element approximately where the input element is
+    // so the cursor doesn't jump around
+    var __tempEl__ = document.createElement('input');
+    __tempEl__.style.position = 'absolute';
+    __tempEl__.style.top = (el.offsetTop + 7) + 'px';
+    __tempEl__.style.left = el.offsetLeft + 'px';
+    __tempEl__.style.height = 0;
+    __tempEl__.style.opacity = 0;
+    // Put this temp element as a child of the page <body> and focus on it
+    document.body.appendChild(__tempEl__);
+    __tempEl__.focus();
+
+    // The keyboard is open. Now do a delayed focus on the target element
+    setTimeout(function() {
+      el.focus();
+      el.click();
+      // Remove the temp element
+      document.body.removeChild(__tempEl__);
+    }, timeout);
+  }
+}
+
 
 export function fieldLabelToggle(labelId) {
   return (event) => {
