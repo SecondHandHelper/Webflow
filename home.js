52c3ca7092484924f1b6105ad52d113ac2f034e7
Mikael Amborn <mikael.amborn@gmail.com>
2024-02-29 14:27:53 +0100

    Set commission cookie for everyone

--- home.js
+++ home.js
@@ -100,6 +100,8 @@ function showNoCommissionCampaign() {
         noCommissionCampaignDiv.style.top = noCommissionCampaignDiv.style.top === '0px' ? '-80px' : '0px';
       }
     }, {rootMargin: '0px 0px -100%'}).observe(document.getElementById('sellItemCtaButton'));
+  } else {
+    setCookie(cookieName, 'commission', 7);
   }
 }
 
