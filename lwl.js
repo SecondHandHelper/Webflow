a2b0c6a3909aa646bc4e8b88a889a256f0f8c165
trosman <tobias.rosman@gmail.com>
2024-04-26 12:01:04 +0200

    lwl styling stuff

--- lwl.js
+++ lwl.js
@@ -168,3 +168,27 @@ const showLwLDraftItems = (draftItems) => {
     itemList.appendChild(newItemCard);
   }
 }
+
+//INTRO STUFF
+document.getElementById('introNext').addEventListener('click', () => {
+  if (document.getElementById('introNext').innerText === 'Sätt igång') {
+    setCookie('lwlIntroSeen', 'true');
+    document.getElementById('introSection').style.display = 'none';
+  }
+  document.getElementById('introRightArrow').click();
+  if (document.querySelector('.w-slider-nav div:last-child').classList.contains('w-active')) {
+    document.getElementById('introNext').innerText = 'Sätt igång';
+  }
+});
+document.getElementById('skipIntro').addEventListener('click', () => {
+  setCookie('lwlIntroSeen', 'true');
+  document.getElementById('introSection').style.display = 'none';
+});
+
+if (getCookie('lwlIntroSeen') !== 'true') {
+  document.getElementById('introSection').style.display='flex';
+}
+
+document.getElementById('openIntroButton').addEventListener('click', () => {
+  document.getElementById('introSection').style.display = 'flex';
+});
