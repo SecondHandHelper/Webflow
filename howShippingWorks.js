b62fe661befb8391f63df5f614b4e2249e7d08a9
Mikael Amborn <mikael.amborn@gmail.com>
2024-06-18 10:41:37 +0200

    Only call maxNumBags if user is logged in

--- howShippingWorks.js
+++ howShippingWorks.js
@@ -1,7 +1,9 @@
 async function showOrderBagsButton() {
-  const maxBags = await callBackendApi('/api/bags/orders/allowed', { requiresAuth: true });
-  if (maxBags?.data?.maxOrderBags > 0) {
-    document.getElementById('orderBagsButton').style.display = 'flex';
+  if (authUser.current) {
+    const maxBags = await callBackendApi('/api/bags/orders/allowed', {requiresAuth: true});
+    if (maxBags?.data?.maxOrderBags > 0) {
+      document.getElementById('orderBagsButton').style.display = 'flex';
+    }
   }
 }
 showOrderBagsButton();
