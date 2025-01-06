71f131411fcee9a88b330a944b7ad2faaa94923f
trosman <tobias.rosman@gmail.com>
2025-01-06 23:16:37 +0100

    updates to brand.js

--- brand.js
+++ brand.js
@@ -20,7 +20,11 @@ function loadRecentlySold() {
 
       for (const item of result.data) {
         const brand = item.brand;
-        const modelName = item.modelName || 'Model Name';
+        const cardSubtitle = item.brand;
+        let cardTitle = item.category;
+        if (item.cleanedModel){
+          cardTitle = `${item.cleanedModel}${item.category ? `, ${item.category}` : ''}`;
+        }
         const soldPrice = item.soldPrice;
         const soldDate = new Date(item.soldDate);
         const publishedDate = new Date(item.publishedDate);
@@ -34,10 +38,11 @@ function loadRecentlySold() {
                         <div class='text-block-34'>${brand}</div>`;
           if (isFilippaK) {
             itemCardHTML = `<div class="div-block-14-super-big"><div class="ratio-box _16-9"><div class="conten-block with-image">
-                        <div class="img-container" style="background-image: url('${imageUrl}');"></div></div></div>
-                        <div class="text-block-14">${modelName}</div>
-                        <div class='text-block-34'>${brand}</div>
-                        <div class='text-block-34'>${soldPrice} kr</div>`;
+                        <div class="img-container" style="background-image: url('${imageUrl}');">
+                        <div class="sold-tag">Sold</div></div></div></div>
+                        <div class="text-block-14">${soldPrice} kr</div>
+                        <div class='text-block-34'>${cardTitle}</div>
+                        <div class='text-block-34'>${cardSubtitle}</div>`;
           }
           //I cut out the "Idag / Igår" during summer, since so little is sold every day. Add this last to show it again: <div class='text-block-34'>${soldTimeText}</div></div>
           itemListRecentlySoldStartPage.innerHTML += itemCardHTML;
