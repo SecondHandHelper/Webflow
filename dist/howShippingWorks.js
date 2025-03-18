1bae156231e4d6f43394b9c523e05d647548aa07
Mikael Amborn <mikael.amborn@gmail.com>
2025-03-18 09:12:20 +0100

    Update item page and add featureFlag trustedSeller

--- dist/howShippingWorks.js
+++ dist/howShippingWorks.js
@@ -1,2 +1,21 @@
-!function(e,n,t,r,o){var i="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{},f="function"==typeof i[r]&&i[r],u=f.cache||{},d="undefined"!=typeof module&&"function"==typeof module.require&&module.require.bind(module);function l(n,t){if(!u[n]){if(!e[n]){var o="function"==typeof i[r]&&i[r];if(!t&&o)return o(n,!0);if(f)return f(n,!0);if(d&&"string"==typeof n)return d(n);var a=Error("Cannot find module '"+n+"'");throw a.code="MODULE_NOT_FOUND",a}s.resolve=function(t){var r=e[n][1][t];return null!=r?r:t},s.cache={};var c=u[n]=new l.Module(n);e[n][0].call(c.exports,s,c,c.exports,this)}return u[n].exports;function s(e){var n=s.resolve(e);return!1===n?{}:l(n)}}l.isParcelRequire=!0,l.Module=function(e){this.id=e,this.bundle=l,this.exports={}},l.modules=e,l.cache=u,l.parent=f,l.register=function(n,t){e[n]=[function(e,n){n.exports=t},{}]},Object.defineProperty(l,"root",{get:function(){return i[r]}}),i[r]=l;for(var a=0;a<n.length;a++)l(n[a]);if(t){var c=l(t);"object"==typeof exports&&"undefined"!=typeof module?module.exports=c:"function"==typeof define&&define.amd?define(function(){return c}):o&&(this[o]=c)}}({kMCfr:[function(e,n,t){!async function(){if(authUser.current){let e=await callBackendApi("/api/bags/orders/allowed",{requiresAuth:!0});e?.data?.maxOrderBags>0&&(document.getElementById("orderBagsButton").style.display="flex")}}()},{}]},["kMCfr"],"kMCfr","parcelRequire81ca");
+!// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+function(e,n,t,r,o){/* eslint-disable no-undef */var i="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{},f="function"==typeof i[r]&&i[r],u=f.cache||{},d="undefined"!=typeof module&&"function"==typeof module.require&&module.require.bind(module);function l(n,t){if(!u[n]){if(!e[n]){// if we cannot find the module within our internal map or
+// cache jump to the current global require ie. the last bundle
+// that was added to the page.
+var o="function"==typeof i[r]&&i[r];if(!t&&o)return o(n,!0);// If there are other bundles on this page the require from the
+// previous one is saved to 'previousRequire'. Repeat this as
+// many times as there are bundles until the module is found or
+// we exhaust the require chain.
+if(f)return f(n,!0);// Try the node require function if it exists.
+if(d&&"string"==typeof n)return d(n);var a=Error("Cannot find module '"+n+"'");throw a.code="MODULE_NOT_FOUND",a}s.resolve=function(t){var r=e[n][1][t];return null!=r?r:t},s.cache={};var c=u[n]=new l.Module(n);e[n][0].call(c.exports,s,c,c.exports,this)}return u[n].exports;function s(e){var n=s.resolve(e);return!1===n?{}:l(n)}}l.isParcelRequire=!0,l.Module=function(e){this.id=e,this.bundle=l,this.exports={}},l.modules=e,l.cache=u,l.parent=f,l.register=function(n,t){e[n]=[function(e,n){n.exports=t},{}]},Object.defineProperty(l,"root",{get:function(){return i[r]}}),i[r]=l;for(var a=0;a<n.length;a++)l(n[a]);if(t){// Expose entry point to Node, AMD or browser globals
+// Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+var c=l(t);// CommonJS
+"object"==typeof exports&&"undefined"!=typeof module?module.exports=c:"function"==typeof define&&define.amd?define(function(){return c}):o&&(this[o]=c)}}({kMCfr:[function(e,n,t){!async function(){if(authUser.current){let e=await callBackendApi("/api/bags/orders/allowed",{requiresAuth:!0});e?.data?.maxOrderBags>0&&(document.getElementById("orderBagsButton").style.display="flex")}}()},{}]},["kMCfr"],"kMCfr","parcelRequire81ca")//# sourceMappingURL=howShippingWorks.js.map
+;
 //# sourceMappingURL=howShippingWorks.js.map
