628d05cf89e2c28635cbd2aa37dc4dd6b9a27d5c
trosman <tobias.rosman@gmail.com>
2025-04-04 12:17:09 +0200

    fixing copy

--- private.js
+++ private.js
@@ -790,16 +790,16 @@ function showTrustedSellerBottomSheet() {
   const change = user.current?.trustedSellerStatusChange.type;
   if (change === 'pendingToTrusted') {
     document.getElementById('trustedSellerTitle').innerText = 'Du har uppnåt statusen\nPålitlig Säljare!';
-    document.getElementById('trustedSellerText').innerText = 'Tack vare en hög andel godkända försäljningar har du nu blivit Pålitlig Säljare! Från och med nu får du utbetalt direkt när du skickar dina plagg och täcks av Mais säljskydd vid eventuella reklamationer.';
+    document.getElementById('trustedSellerText').innerText = 'Tack vare en hög andel godkända försäljningar har du nu blivit Pålitlig Säljare! Från och med nu får du utbetalt direkt när du skickar dina plagg och täcks av Mais säljarskydd vid eventuella reklamationer.';
   } else if (change === 'untrustedToTrusted') {
     document.getElementById('trustedSellerTitle').innerText = 'Du har uppnåt statusen\nPålitlig Säljare!';
-    document.getElementById('trustedSellerText').innerText = `Tack${user.current?.addressFirstName ? ' ' + user.current.addressFirstName : ''}, för att du gör det enkelt för köpare att handla tryggt i andra hand! Från och med nu får du utbetalt direkt när du skickar dina plagg och täcks av Mais säljskydd vid eventuella reklamationer.`;
+    document.getElementById('trustedSellerText').innerText = `Tack${user.current?.addressFirstName ? ' ' + user.current.addressFirstName : ''}, för att du gör det enkelt för köpare att handla tryggt i andra hand! Från och med nu får du utbetalt direkt när du skickar dina plagg och täcks av Mais säljarskydd vid eventuella reklamationer.`;
   } else if (change === 'pendingToUntrusted') {
     document.getElementById('trustedSellerButton').innerText = 'Okej';
     document.getElementById('trustedSellerIcon').style.opacity = '0.2';
     document.getElementById('trustedSellerLink').innerText = 'Hur blir jag Pålitlig Säljare?'
     document.getElementById('trustedSellerTitle').innerText = 'Just nu uppfyller du inte kriterierna för Pålitlig Säljare';
-    document.getElementById('trustedSellerText').innerHTML = `Eftersom en av dina första försäljningar antingen reklamerades eller inte skickades i tid, så uppfyller du just nu inte kriterierna för statusen Pålitlig Säljare. Du kan fortfarande sälja, men har ännu inte kvalificerat dig för snabbare utbetalningar eller Mais säljskydd.`;
+    document.getElementById('trustedSellerText').innerHTML = `Eftersom en av dina första försäljningar antingen reklamerades eller inte skickades i tid, så uppfyller du just nu inte kriterierna för statusen Pålitlig Säljare. Du kan fortfarande sälja, men har ännu inte kvalificerat dig för snabbare utbetalningar eller Mais säljarskydd.`;
   } else if (change === 'trustedToUntrusted') {
     document.getElementById('trustedSellerButton').innerText = 'Okej';
     document.getElementById('trustedSellerIcon').style.opacity = '0.2';
