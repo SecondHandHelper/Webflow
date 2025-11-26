f9a5edfd053caa36eefdca7f1eda2be909c425b2
trosman <tobias.rosman@gmail.com>
2025-11-26 11:06:34 +0100

    fixing my own code

--- shipItem.js
+++ shipItem.js
@@ -78,11 +78,10 @@ function loadItem(itemId) {
                 openShippingLabelButton.href = vestiaireShippingLabel;
                 openShippingLabelButton.style.display = 'block';
               }
-            else if (soldPlatform === 'Grailed') {
+            } else if (soldPlatform === 'Grailed') {
               mainInstructionText.innerText = 'Sätt på fraktsedeln vi skickat dig på påsen och lämna till ombud';
               howToShipNoQrDiv.style.display = "block";
               mainInstructionDiv.style.display = "block";
-            }
             } else if (!postnordQrCode && !(soldPlatform === 'Vestiaire Collective' || soldPlatform === 'Grailed')) {
               howToShipQrDiv.style.display = 'block';
               postnordQrCodeMissingDiv.style.display = 'flex';
