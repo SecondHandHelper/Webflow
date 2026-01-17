b6df632e40cc79853278a29750c4c0023322d971
trosman <tobias.rosman@gmail.com>
2026-01-17 22:56:43 +0100

    fixing the spinning bug

--- private.js
+++ private.js
@@ -6,13 +6,13 @@ var userId;
 var email;
 var phone;
 
-async function initAppDownloadBanner() {
-  const customToken = await callBackendApi('/api/users/token', { method: 'POST', requiresAuth: true });
-  document.getElementById('openAppAndSignIn').href = 'maiapp://?aat=' + encodeURIComponent(customToken.data.customToken);
-}
+// async function initAppDownloadBanner() {
+//   const customToken = await callBackendApi('/api/users/token', { method: 'POST', requiresAuth: true });
+//   document.getElementById('openAppAndSignIn').href = 'maiapp://?aat=' + encodeURIComponent(customToken.data.customToken);
+// }
 
-initAppDownloadBanner();
-setInterval(initAppDownloadBanner, 10 * 60 * 1000);
+// initAppDownloadBanner();
+// setInterval(initAppDownloadBanner, 10 * 60 * 1000);
 
 export function updateIC(userId, em, ph) {
   let email = em;
